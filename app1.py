54b0da8501556570623c072380978ea9e82a245c
suho9soft <94418346+suho9soft@users.noreply.github.com>
2025-01-06 03:15:04 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,8 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 import mysql.connector
 from mysql.connector import Error
-import streamlit as st
-import threading
 
 # MySQL 데이터베이스에 데이터 삽입 함수
 def insert_data(num, name, age, gender):
@@ -22,15 +20,15 @@ def insert_data(num, name, age, gender):
             cursor.execute("SELECT COUNT(*) FROM user WHERE num = %s", (num,))
             result = cursor.fetchone()
             if result[0] > 0:
-                st.error(f"번호 {num} 는 이미 존재합니다. 다른 번호를 입력해 주세요.")
+                print(f"번호 {num} 는 이미 존재합니다. 다른 번호를 입력해 주세요.")
                 return
 
             query = "INSERT INTO user (num, name, age, gender) VALUES (%s, %s, %s, %s)"
             cursor.execute(query, (num, name, age, gender))
             connection.commit()
-            st.success("데이터가 성공적으로 삽입되었습니다.")
+            print("데이터가 성공적으로 삽입되었습니다.")
     except Error as e:
-        st.error(f"Error: '{e}'")
+        print(f"Error: '{e}'")
     finally:
         if connection and connection.is_connected():
             cursor.close()
@@ -50,7 +48,7 @@ def print_input():
         insert_data(num, name, age, gender)
         print(f"번호: {num}, 이름: {name}, 나이: {age}, 성별: {gender}")
     except ValueError:
-        st.error("번호와 나이는 숫자로 입력해 주세요.")
+        print("번호와 나이는 숫자로 입력해 주세요.")
 
 # Tkinter 윈도우 설정 함수
 def tkinter_app():
@@ -87,20 +85,5 @@ def tkinter_app():
     # Tkinter 윈도우 실행
     root.mainloop()
 
-# Streamlit 앱 함수
-def streamlit_app():
-    st.title("서버 주소 입력 폼")
-    
-    # 서버 주소 입력 받기
-    server_url = st.text_input('서버 주소를 입력하세요:', 'http://172.30.1.177:8501')
-
-    # 서버로 이동할 링크 생성 버튼
-    if st.button('서버로 이동하기'):
-        st.markdown(f'[서버로 이동하기]({server_url})', unsafe_allow_html=True)
-
-# Streamlit 애플리케이션을 별도의 스레드에서 실행
-streamlit_thread = threading.Thread(target=streamlit_app)
-streamlit_thread.start()
-
-# Tkinter 애플리케이션 실행
-tkinter_app()
+if __name__ == "__main__":
+    tkinter_app()
